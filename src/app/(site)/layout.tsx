8299a7a75680535866d80d865d9b55f18e0defc9
geoffray <geoffray.atlan@gmail.com>
2025-07-13 15:07:09 +0200

    test gtm

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -33,48 +33,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="icon" href="/favicon.ico" />
 
-        {/* Préchargement optimisé LCP */}
-        {/* <link
-          rel="preload"
-          as="image"
-          href="https://res.cloudinary.com/dx96rdxwk/image/upload/v1748531998/Mon%20projet%20locatif/Header-mobil_tbfewv.webp?w=400"
-          imageSrcSet="
-            https://res.cloudinary.com/dx96rdxwk/image/upload/v1748531998/Mon%20projet%20locatif/Header-mobil_tbfewv.webp?w=400 1x,
-            https://res.cloudinary.com/dx96rdxwk/image/upload/v1748531998/Mon%20projet%20locatif/Header-mobil_tbfewv.webp?w=800 2x
-          "
-          imageSizes="(max-width: 639px) 100vw"
-          fetchPriority="high"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          as="image"
-          href="https://res.cloudinary.com/dx96rdxwk/image/upload/v1748531997/Mon%20projet%20locatif/Header-tablet_edp2mu.webp?w=768"
-          imageSrcSet="
-            https://res.cloudinary.com/dx96rdxwk/image/upload/v1748531997/Mon%20projet%20locatif/Header-tablet_edp2mu.webp?w=768 1x,
-            https://res.cloudinary.com/dx96rdxwk/image/upload/v1748531997/Mon%20projet%20locatif/Header-tablet_edp2mu.webp?w=1536 2x
-          "
-          imageSizes="(min-width: 640px) and (max-width: 767px) 100vw"
-          fetchPriority="high"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          as="image"
-          href="https://res.cloudinary.com/dx96rdxwk/image/upload/v1748531765/Mon%20projet%20locatif/Header-desktop_rtmbza.webp?w=1200"
-          imageSrcSet="
-            https://res.cloudinary.com/dx96rdxwk/image/upload/v1748531765/Mon%20projet%20locatif/Header-desktop_rtmbza.webp?w=1200 1x,
-            https://res.cloudinary.com/dx96rdxwk/image/upload/v1748531765/Mon%20projet%20locatif/Header-desktop_rtmbza.webp?w=2400 2x
-          "
-          imageSizes="(min-width: 768px) 1280px"
-          fetchPriority="high"
-          crossOrigin="anonymous"
-        /> */}
-
         <link rel="preconnect" href="https://www.googletagmanager.com" />
-        <GTM />
       </Head>
       <body className={inter.className}>
+        <GTM />
         <NextTopLoader
           color="#006BFF"
           crawlSpeed={300}
--- src/components/Pixels/GTMClient.tsx
+++ src/components/Pixels/GTMClient.tsx
@@ -8,7 +8,7 @@ export function GTM() {
 
   return (
     <>
-      {/* 1. Data Layer init */}
+      {/* 1. Init Data Layer */}
       <Script id="gtm-init" strategy="lazyOnload">
         {`
           window.dataLayer = window.dataLayer || [];
@@ -19,22 +19,24 @@ export function GTM() {
         `}
       </Script>
 
-      {/* 2. Chargement du gtm.js */}
+      {/* 2. Script principal */}
       <Script
         id="gtm-script"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtm.js?id=${id}`}
       />
 
-      {/* Fallback noscript déplacé ici */}
-      <noscript>
-        <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        />
-      </noscript>
+      {/* 3. Fallback <noscript> : à rendre dans le DOM visible */}
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=${id}"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          </noscript>
+        `,
+        }}
+      />
     </>
   );
 }
